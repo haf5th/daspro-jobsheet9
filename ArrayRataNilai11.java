896a773b4c70c0395a7d577c3c3d1b60239642a8
haf5th <hafiflearning@gmail.com>
2024-10-31 23:24:02 +0700

    Percobaan 3

--- ArrayRataNilai11.java
+++ ArrayRataNilai11.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 public class ArrayRataNilai11 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int [] nilaiMhs= new int [10];
+
+        System.out.println("Masukkan jumlah mahasiswa: ");
+        int jumlahMhs = sc.nextInt();
+
+        int [] nilaiMhs= new int [jumlahMhs];
         double total= 0;
-        double rata2;
+        double rata2,lulus=0,tidaklulus=0,ratalulu=0,ratatidaklulus=0,jmlLulus=0,jmlTidakLulus=0;
 
         for (int i= 0; i<nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
@@ -12,9 +16,20 @@ public class ArrayRataNilai11 {
         }
         for (int i= 0; i<nilaiMhs.length; i++){
             total += nilaiMhs[i];
+            if (nilaiMhs[i]>70){
+                lulus += nilaiMhs[i];
+                jmlLulus++;
+            }else {
+                tidaklulus += nilaiMhs[i];
+                jmlTidakLulus++;
+            }
         }
         rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+        ratalulu = lulus/jmlLulus;
+        ratatidaklulus = tidaklulus/jmlTidakLulus;
+
+        System.out.println("Rata-rata mahasiswa lulus = "+ ratalulu);
+        System.out.println("Rata-rata nilai tidak lulus = "+ ratatidaklulus);
 
     }
 }

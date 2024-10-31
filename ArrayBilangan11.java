24274aac4316608afa1c7df18e0942677e832862
haf5th <hafiflearning@gmail.com>
2024-10-31 22:20:18 +0700

    Percobaan 1

--- ArrayBilangan11.java
+++ ArrayBilangan11.java
@@ -1,9 +1,9 @@
 public class ArrayBilangan11{
     public static void main(String[] args) {
         int [] bil = {5,3,-7,17};
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
